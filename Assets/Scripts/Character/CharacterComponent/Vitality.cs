c9258f785e8c8fe8e17aa534a848c869ee91fa28
Nicolas-Bisson <nicolasbisson14@gmail.com>
2021-02-26 14:35:02 -0500

    Adds basic character with health and damage value

---
+++ Assets/Scripts/Character/CharacterComponent/Vitality.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vitality : MonoBehaviour
+{
+    [SerializeField] private int maxHealth = 10;
+
+    private int health;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
