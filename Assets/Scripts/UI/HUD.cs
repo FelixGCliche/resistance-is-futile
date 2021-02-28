981d4018303c6cbb87687c34313b51a531fd27b6
Antony Ménard <antonymenard2014@gmail.com>
2021-02-27 20:14:44 -0500

    Main & pause menu

--- Assets/Scripts/UI/HUD.cs
+++ Assets/Scripts/UI/HUD.cs
@@ -19,7 +19,6 @@ public class HUD : MonoBehaviour
     {
         StartCoroutine(WaitForEnabledEnemy());
     }
-    
 
     private IEnumerator WaitForEnabledEnemy()
     {
---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("SampleScene");
+        SceneManager.UnloadSceneAsync("MainMenuScene");
+    }
+    
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
